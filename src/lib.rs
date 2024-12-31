e628794d2492bd292b4da2b702bcc1b235bc358a
Max Ignatyev <mxxnsk@gmail.com>
2024-12-30 22:01:45 -0800

    add roblox-install bindings

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,24 @@
 
 use napi_derive::napi;
 
+use roblox_install::RobloxStudio;
+
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+pub fn studio_plugins_path() -> String {
+    let studio = RobloxStudio::locate().unwrap();
+    let plugins = studio.plugins_path();
+
+    let result = plugins.to_str().unwrap();
+
+    return result.to_string();
+}
+
+#[napi]
+pub fn studio_content_path() -> String {
+    let studio = RobloxStudio::locate().unwrap();
+    let content = studio.content_path();
+
+    let result = content.to_str().unwrap();
+
+    return result.to_string();
 }
