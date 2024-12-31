e359fedd50cba6f926c7b491e26bb9a9a69ef576
Max Ignatyev <mxxnsk@gmail.com>
2024-12-30 22:03:51 -0800

    change formatting

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,20 @@ use roblox_install::RobloxStudio;
 
 #[napi]
 pub fn studio_plugins_path() -> String {
-    let studio = RobloxStudio::locate().unwrap();
-    let plugins = studio.plugins_path();
+  let studio = RobloxStudio::locate().unwrap();
+  let plugins = studio.plugins_path();
 
-    let result = plugins.to_str().unwrap();
+  let result = plugins.to_str().unwrap();
 
-    return result.to_string();
+  return result.to_string();
 }
 
 #[napi]
 pub fn studio_content_path() -> String {
-    let studio = RobloxStudio::locate().unwrap();
-    let content = studio.content_path();
+  let studio = RobloxStudio::locate().unwrap();
+  let content = studio.content_path();
 
-    let result = content.to_str().unwrap();
+  let result = content.to_str().unwrap();
 
-    return result.to_string();
+  return result.to_string();
 }
