312357626dd344fa3508393e4b639261657b27a9
Max Ignatyev <mxxnsk@gmail.com>
2025-01-03 19:53:48 -0800

    0.0.5

---
+++ package/main.mjs
@@ -0,0 +1,4 @@
+import { studioPluginsPath, studioContentPath } from './index.js'
+ 
+console.log('Studio plugins path from JS:', studioPluginsPath())
+console.log('Studio content path from JS:', studioContentPath())
